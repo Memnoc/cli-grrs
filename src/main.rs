dd5ab1ae0cb254ef04f00245ee9c41ba42c11ccd
Memnoc <smartdroidesign@gmail.com>
2024-04-20 08:47:02 +0100

    parsing args and path with clap and iterating over file

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,          // pattern to look for
+    path: std::path::PathBuf, // path of file to read
+}
+
+fn main() {
+    println!("== GRRS ==");
+
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
