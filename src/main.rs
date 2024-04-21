e4bd3e10329e2b1634753dffb47c16fc78a4a7ea
Memnoc <smartdroidesign@gmail.com>
2024-04-21 17:57:59 +0100

    following the original implementation with read_to_string

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use clap::Parser;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -11,36 +9,13 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
-    // Open the file specified in the path
-    let file = match File::open(&args.path) {
-        Ok(file) => file,
-        Err(e) => {
-            return Err(io::Error::new(
-                e.kind(),
-                format!("Failed to open the file '{}': {}", args.path.display(), e),
-            ))
-        }
-    };
-
-    let reader = BufReader::new(file);
-
-    // Read the file line by line
-    for line_result in reader.lines() {
-        let line = match line_result {
-            Ok(line) => line,
-            Err(e) => {
-                return Err(io::Error::new(
-                    e.kind(),
-                    "Error reading a line from tne file",
-                ))
-            }
-        };
+    for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
-    Ok(())
 }
