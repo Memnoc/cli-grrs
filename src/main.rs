7085f85f2269a3bd53017043605c8ebddd5b25fd
Memnoc <smartdroidesign@gmail.com>
2024-04-22 07:00:29 +0100

    wrote some testing for matching function

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn find_matches(content: &str, pattern: &str) {
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     let pb = indicatif::ProgressBar::new(content.lines().count() as u64);
     for line in content.lines() {
         if line.contains(pattern) {
+            writeln!(writer, "{}", line).unwrap();
             pb.println(format!("[+] finished #{}", line));
             pb.inc(1);
         }
@@ -23,10 +24,17 @@ fn find_matches(content: &str, pattern: &str) {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    find_matches(&content, &args.pattern);
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
     Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
