a7d684e4fe36f28aacdda00bb26201407e43fa93
Memnoc <smartdroidesign@gmail.com>
2024-04-22 06:38:41 +0100

    moved parsing to a function

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,24 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+fn find_matches(content: &str, pattern: &str) {
+    let pb = indicatif::ProgressBar::new(content.lines().count() as u64);
+    for line in content.lines() {
+        if line.contains(pattern) {
+            pb.println(format!("[+] finished #{}", line));
+            println!("{}", line);
+        }
+        pb.inc(1);
+        pb.finish_with_message("done");
+    }
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
 
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    // NOTE: progress bar
-    let pb = indicatif::ProgressBar::new(content.lines().count() as u64);
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            pb.println(format!("[+] finished #{}", line));
-            println!("{}", line);
-        }
-    }
-    pb.finish_with_message("done");
+    find_matches(&content, &args.pattern);
     Ok(())
 }
