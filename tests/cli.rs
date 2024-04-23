dd61cff571c60c5a8ed9a54bb60bcd403dbc9abd
Memnoc <smartdroidesign@gmail.com>
2024-04-23 06:56:08 +0100

    wrote some integration testing for empty file content

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*;
+use assert_fs::fixture::FileWriteStr;
 use predicates::prelude::*;
 use std::process::Command;
 
@@ -10,6 +11,33 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+}
+
+#[test]
+fn find_empty_file() -> Result<(), Box<dyn std::error::Error>> {
+    let temp_file = assert_fs::NamedTempFile::new("empty_sample.txt")?;
+    temp_file.write_str("")?;
+    println!("Path: {:?}", temp_file.path());
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(temp_file.path());
+    cmd.assert().success().stdout(predicate::str::contains(""));
 
     Ok(())
 }
